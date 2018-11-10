d9d46e8a22763b309926ca7643c9628b42d2a97e
Nick Christensen <nchristensen@homeaway.com>
2018-11-10 17:22:24 -0600

    Reduce control size

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const SPACING = 4;
-export const FONT_SIZE = 8;
-export const BUTTON_PADDING = 2;
+export const FONT_SIZE = 6;
+export const BUTTON_PADDING = 3;
 
 export const MIN_SPEED = 10;
 export const MAX_SPEED = 100;
